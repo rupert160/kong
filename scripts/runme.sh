a6617a2f0592f91de2c40cfc5e8c62870e5a89be
Rupert Bailey <rupert@learningmethod.com.au>
2019-10-10 21:33:21 +1100

    modified with kong container and setup script

---
+++ scripts/runme.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+PROJECT_DIR="$(cd "`dirname $0`"/..; pwd)"
+DOWNLOADS_DIR="${PROJECT_DIR}/downloads"
+BINARY_DIR="${PROJECT_DIR}/bin"
+TFSPEC_DIR="${PROJECT_DIR}/infrastructure/deployment-pipeline/"
+
+TERRAFORM_VERSION='0.12.10'
+TERRAFORM_ZIP="terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+TERRAFORM_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/${TERRAFORM_ZIP}"
+TERRAFORM_BIN='terraform'
+
+function exit_error(){
+   echo "$1"
+   exit 1
+}
+
+function export_aws_credentials(){
+  echo "Warning - using your default AWS credentials..."
+
+  if ! [[ -r "${HOME}/.aws/credentials" ]]; then
+     exit_error "error: ${HOME}/.aws/credentials needs to be readable" 
+  fi
+  
+  if ! [[ -r "${HOME}/.aws/config" ]]; then
+     exit_error "error: ${HOME}/.aws/config needs to be readable" 
+  fi
+  
+  $(sed -n '/\[default\]/,/\[/s/^\s*//p' ~/.aws/{config,credentials} | \
+  	grep --color=never 'access_key\|region' | \
+  	sed 's/region/aws_default_region/' | \
+  	sed -e 's/\(.*\)=\(.*\)/export \U\1=\E\2/'
+  )
+}
+
+function install_terraform(){
+   mkdir -p "$PROJECT_DIR" "${DOWNLOADS_DIR}" "${BINARY_DIR}"
+   cd "$PROJECT_DIR"
+   
+   if ! [[ -f "${DOWNLOADS_DIR}/${TERRAFORM_ZIP}" ]]; then
+      wget --directory-prefix="${DOWNLOADS_DIR}" "${TERRAFORM_URL}"
+   fi
+   
+   rm "$BINARY_DIR/$TERRAFORM_BIN"
+   unzip -d "$BINARY_DIR" "${DOWNLOADS_DIR}/${TERRAFORM_ZIP}"
+   
+   kong_terraform="${BINARY_DIR}/${TERRAFORM_BIN}"
+
+   chmod u+x "${kong_terraform}"
+   alias t="${kong_terraform}"
+   
+   if ! [[ -x "${kong_terraform}" ]]; then
+      exit_error "error: terreform binary is not executable" 
+   fi
+   t version
+}
+
+function setup_codecommit(){
+   echo "follow ssh instructions here before proceeding..."
+   echo "https://docs.aws.amazon.com/codecommit/latest/userguide/setting-up-ssh-unixes.html#setting-up-ssh-unixes-keys"
+   
+   echo "have you setup ~/.ssh/config with a \"Host git-codecommit.*.amazonaws.com\" entry?"
+   select yn in "Yes" "No"; do
+       case $yn in
+           Yes ) git remote add codecommit ssh://git-codecommit.ap-southeast-2.amazonaws.com/v1/repos/docker-image-build; break;;
+           No ) exit;;
+       esac
+   done
+}
+function change_aws_account(){
+   echo "have you amended your account the project_root/buildspec.yml from 735655096069 file?"
+   select yn in "Yes" "No"; do
+       case $yn in
+           Yes ) break;;
+           No ) exit;;
+       esac
+   done
+}
+
+function tear_down(){
+   t plan -destroy -out=destroy-plan
+   t plan apply destroy-plan
+   t apply destroy-plan
+}
+
+function kong_config(){
+   export_aws_credentials
+   install_terraform
+   setup_codecommit
+   change_aws_account
+}
+
+function kong_build(){
+   cd ${TFSPEC_DIR}
+   #t 0.12upgrade
+   t init .
+   t apply -auto-approve
+   git push --set-upstream codecommit master
+}
+
+function kong_deploy(){
+   
+}
